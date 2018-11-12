e454bad289036f1ebb0ee1f30245fe0762fd5653
vitta101 <vitta101@mail.chapman.edu>
2018-11-12 13:09:07 -0800

    Edits Trendify.py

--- Trendify.py
+++ Trendify.py
@@ -11,8 +11,25 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 #------------------------------------------------------------------------
 #User inputs artist name and prints popular tracks
-artist_name = input("Enter an artist name to print their popular tracks: ")
+artist_name = input("Enter an artist name to display their popular tracks: ")
 results = sp.search(q=artist_name, limit=20)
 for i, t in enumerate(results['tracks']['items']):
     print(' ', i, t['name'])
-#------------------------------------------------------------------------
+
+print("--------------------------------------------------------")
+
+#User inputs artist name and prints related artists
+artist_name = input("Enter an artist name to display related artists: ")
+result = sp.search(q='artist:' + artist_name, type='artist')
+try:
+    name = result['artists']['items'][0]['name']
+    uri = result['artists']['items'][0]['uri']
+
+    related = sp.artist_related_artists(uri)
+    print('Related artists for', name)
+    for artist in related['artists']:
+        print('  ', artist['name'])
+except:
+    print("usage show_related.py [artist-name]")
+
+print("--------------------------------------------------------")
